ef209ad2f4e3b4b7608f3b3d82aa1cc30346030d
izerhkaf <farhanfakhrezi@gmail.com>
2021-05-30 21:09:12 +0700

    login-onprogress

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../config/axios.js'
 
 Vue.use(Vuex)
 
@@ -9,6 +10,22 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    login (context, payload) {
+      axios({
+        method: 'POST',
+        url: '/login',
+        data: {
+          identity: payload.identity,
+          password: payload.password
+        }
+      })
+        .then(({ data }) => {
+          console.log(data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
   },
   modules: {
   }
---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="container col-4 mt-4 login rounded">
+    <h1>Login</h1>
+    <form>
+      <div class="my-3">
+        <label class="form-label">Username or Email</label>
+        <input type="text" class="form-control" v-model="identity">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" class="form-control" v-model="password">
+      </div>
+      <div class="mb-3">
+        <router-link to="/register">Create New Account</router-link>
+      </div>
+      <button type="submit" class="btn btn-success mb-4" @click.prevent="login">Login</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      identity: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$store.dispatch('login', {
+        identity: this.identity,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
