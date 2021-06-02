27e3bf54b5584716e3dab6318da0be5387214bd9
rizanopriatmoko <rizanopriatmoko98@gmail.com>
2021-06-02 12:06:41 +0700

    footer change

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -5,7 +5,8 @@
         class="my-5 text-center"
         style="color: blanchedalmond; font-family: serif; font-style: oblique"
       >
-        © 2021 Makanology. All rights reserved.
+        © 2021 Makanology. <br>
+        DISCLAIMER: All the pictures featured on this page belong to their respective owners. I do not own any of these pictures
       </p>
     </div>
   </div>
