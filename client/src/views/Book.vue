85d804b2d2085a93d2c5c92487c3fb44ef55664e
rizanopriatmoko <rizanopriatmoko98@gmail.com>
2021-06-02 14:16:28 +0700

    book-page ok

--- client/src/views/Book.vue
+++ client/src/views/Book.vue
@@ -1,23 +1,65 @@
 <template>
-<div>
-    <navBar/>
-    <div class="container">
-        <div class="row my-5">
-            <div class="col-12 my-5">
-                <div class="card my-5" style="width: 18rem;">
-                    <div class="card-body shadow">
-                        <h5 class="card-title">BOOK FORM</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
+  <div :style="
+          { backgroundImage: 'url(' + require('@/Picture/bookback.jpg') + ')' }
+        "
+        style="
+      background-size: 2000px;
+    ">
+    <navBar />
+    <div class="container" >
+      <div class="row" >
+        <div class="col-12" style="margin-top: 11rem; margin-bottom: 5rem">
+          <div class="card mx-auto" style="width: 40rem">
+            <div class="card-body shadow">
+              <h5 class="card-title">BOOK FORM</h5>
+              <form>
+                <div class="form-group my-3">
+                  <label for="exampleFormControlInput1">Email address</label>
+                  <input
+                    type="email"
+                    class="form-control my-3"
+                    id="exampleFormControlInput1"
+                    placeholder="name@example.com"
+                  />
                 </div>
+                <div class="form-group my-3">
+                  <label for="exampleFormControlInput1">Name</label>
+                  <input
+                    class="form-control my-3"
+                    id="exampleFormControlInput1"
+                  />
+                </div>
+                <div class="form-group my-3">
+                  <label for="exampleFormControlSelect1">People</label>
+                  <select class="form-control my-3" id="exampleFormControlSelect1">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                  </select>
+                </div>
+                <div class="form-group my-3">
+                  <label for="exampleFormControlTextarea1"
+                    >Notes</label
+                  >
+                  <textarea
+                    class="form-control my-3"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    placeholder="e.g: Near Window, Smoking Area"
+                  ></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-    <Footer/>
-</div>
+    <Footer />
+  </div>
 </template>
 
 <script>
@@ -33,5 +75,4 @@ export default {
 </script>
 
 <style>
-
 </style>
