52fc2937f6922a5207cff897d227077961e36659
rizanopriatmoko <rizanopriatmoko98@gmail.com>
2021-06-02 13:38:39 +0700

    book page sementara

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Book from '../views/Book'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/menu',
-    name: 'Menu',
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Menu.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/book',
+    name: 'Book',
+    component: Book
   }
 ]
 
--- client/src/views/Book.vue
+++ client/src/views/Book.vue
@@ -1,21 +1,34 @@
 <template>
 <div>
     <navBar/>
-    
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-12 my-5">
+                <div class="card my-5" style="width: 18rem;">
+                    <div class="card-body shadow">
+                        <h5 class="card-title">BOOK FORM</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <Footer/>
 </div>
-  
 </template>
 
 <script>
 import navBar from '../components/Navbar'
 import Footer from '../components/Footer'
 export default {
-    name: 'Book',
-    components: {
-        navBar,
-        Footer
-    }
+  name: 'Book',
+  components: {
+    navBar,
+    Footer
+  }
 }
 </script>
 
