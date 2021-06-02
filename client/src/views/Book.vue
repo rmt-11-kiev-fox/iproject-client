3ff86b27226ef8246f60ce523284b63479803753
rizanopriatmoko <rizanopriatmoko98@gmail.com>
2021-06-02 13:21:50 +0700

    book initial commit

---
+++ client/src/views/Book.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <navBar/>
+    
+    <Footer/>
+</div>
+  
+</template>
+
+<script>
+import navBar from '../components/Navbar'
+import Footer from '../components/Footer'
+export default {
+    name: 'Book',
+    components: {
+        navBar,
+        Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
