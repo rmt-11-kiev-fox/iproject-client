aa6141fbfe8a26989f2d86252552b15c112ece59
rizanopriatmoko <rizanopriatmoko98@gmail.com>
2021-06-02 17:26:46 +0700

    admin login page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -29,6 +29,14 @@ const routes = [
     path: '/book',
     name: 'Book',
     component: Book
+  },
+  {
+    path: '/login',
+    name: 'AdminLogin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
   }
 ]
 
---
+++ client/src/views/login.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="container-fluid" :style="
+          { backgroundImage: 'url(' + require('@/Picture/menubackground.jpg') + ')' }
+        ">
+      <div class="row" >
+        <div class="col-12" style="margin-top: 11rem; margin-bottom: 5rem">
+          <div class="card mx-auto px-3 py-5" style="width: 40rem; margin-top: 7rem; margin-bottom:11.45rem; padding-top: 2rem;">
+            <div class="card-body">
+              <h5 class="card-title text-center">MAKANOLOGY ADMIN LOGIN</h5>
+              <form>
+                <div class="form-group my-3">
+                  <label for="exampleFormControlInput1">Username</label>
+                  <input
+                    class="form-control my-3"
+                    id="exampleFormControlInput1"
+                  />
+                </div>
+                <div class="form-group my-3">
+                  <label for="exampleFormControlInput1">Password</label>
+                  <input
+                    type="password"
+                    class="form-control my-3"
+                    id="exampleFormControlInput1"
+                  />
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AdminLogin'
+}
+</script>
+
+<style>
+
+</style>
