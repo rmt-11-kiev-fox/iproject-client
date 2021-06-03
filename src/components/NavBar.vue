94142eae77203dbfa0481fd72fb545a84ac4d529
diorvalend55 <w.valendrick@gmail.com>
2021-06-03 14:03:31 +0800

    revisi

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,7 +14,7 @@
 
     <div class="font-sans text-black flex items-center justify-center">
       <div class="border rounded overflow-hidden flex">
-        <input v-model="query" type="text" id="note-textarea" class="px-4 py-2" placeholder="Search...">
+        <input v-model="query" type="text" id="note" class="px-4 py-2" placeholder="Search...">
         <button @click="search(query)" class="flex items-center justify-center px-4 border-l">
           <svg class="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
         </button>
--- src/components/Twitter-Card.vue
+++ src/components/Twitter-Card.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
     <!--Card-->
-    <div class="cardd cover rounded overflow-hidden shadow-lg">
-        <div class="font-bold text-xl mb-2">{{result.text}}</div>
-        <!-- <div class="link px-6 pt-4 pb-2">
-          <a :href="result.url" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Read More</a>
-        </div> -->
+    <div class="col-md-4 cover">
+      <img class="thumbnail" :src="result.thumbnail" onerror="this.onerror=null; this.src='https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg'">
+      <div class="card px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{result.text.substring(0, 100)}}
+        <span v-if="result.text.length >20">...</span>
       </div>
+      <div class="link px-6 pt-4 pb-2">
+          <a :href="result.url" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Read More</a>
+        </div>
+    </div>
   </div>
+
 </template>
 
 <script>
