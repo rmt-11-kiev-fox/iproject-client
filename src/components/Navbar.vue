d33c015866c5d5476e9174b8769631a18c6bc398
Hanun Shahreza <hanunshahreza@Hanuns-MacBook-Air.local>
2021-06-03 13:28:24 +0700

    initialization

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/"> Personal Expenses Assistant </router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul v-if="loggedIn" class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/income"> +Add </router-link>
+          <router-link class="nav-link" to="/reports"> Show Reports </router-link>
+          <router-link class="nav-link" to="/login"> Login </router-link>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0" @submit.prevent="logout">
+        <button v-if="loggedIn" class="btn btn-danger my-2 my-sm-0" type="submit"> Logout </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row d-flex">
+      <div class="col-5 my-3 mx-auto shadow p-3 mb-5 bg-white rounded">
+        <form class="text-center">
+          <h1 class="h3 mb-3 fw-normal">Please Login</h1><br>
+          <div class="form-floating">
+            <label class="text-center" for="username">Username</label>
+            <input type="text" class="form-control" placeholder="Your username here">
+          </div><br>
+          <div class="form-floating">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" placeholder="Your password here">
+          </div><br>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button><br><br>
+          <p> Don't have an account? <a class="btn btn-default btn-dark"> Register </a> </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+
+</style>
