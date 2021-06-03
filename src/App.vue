4cbba939cfdd74f6e0033f97908187faeac4df3c
Jed29 <imnotjeed@gmail.com>
2021-06-04 04:20:26 +0700

    04

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
   <div id="app">
     <Navbar/>
     <router-view/>
-
   </div>
 </template>
 <script>
-import HFooter from 'vue-hacktiv8-footer'
 import Navbar from './components/Navbar'
 export default {
   name:"App",
   components:{
-    Navbar,
-    HFooter
+    Navbar
   }
 }
 </script>
