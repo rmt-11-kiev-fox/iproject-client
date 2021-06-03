4385bf6a7f94ecc3f02d470ab5783efbc518876c
azri fathul <azrifathulabar@gmail.com>
2021-06-03 11:09:40 +0700

    change api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const instanceAxios = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://lelangaja.herokuapp.com/'
 })
 
 module.exports = { instanceAxios }
