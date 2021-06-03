654bb4c13ed8c7ed8af12fbbd3aef4cc79b8433d
Novizh <novizhreload1@gmail.com>
2021-06-03 12:40:12 +0700

    minor revision on navguard

--- src/api/server.js
+++ src/api/server.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000' // development environtment
-    // baseURL: '' // production environtment
+    // baseURL: 'http://localhost:3000' // development environtment
+    baseURL: 'https://novizh-medicio.herokuapp.com' // production environtment
 })
 
 export default instance
