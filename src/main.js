7714e39b5b9fd04d2457deeb4d66672d400cec4c
Novizh <novizhreload1@gmail.com>
2021-06-03 12:19:37 +0700

    essential features done

--- src/main.js
+++ src/main.js
@@ -2,24 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import './assets/css/style.css'
 import './assets/js/main'
-// import './assets/vendor/animate.css/animate.min.css'
-// import './assets/vendor/aos/aos.css'
 import './assets/vendor/bootstrap/css/bootstrap.min.css'
-// import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
-// import './assets/vendor/boxicons/css/boxicons.min.css'
-// import './assets/vendor/fontawesome-free/css/all.min.css'
-// import './assets/vendor/glightbox/css/glightbox.min.css'
-// import './assets/vendor/swiper/swiper-bundle.min.css'
-// import './assets/vendor/aos/aos.js'
-// import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
-// import './assets/vendor/glightbox/js/glightbox.min.js'
-// import './assets/vendor/php-email-form/validate.js'
-// import './assets/vendor/purecounter/purecounter.js'
-// import './assets/vendor/swiper/swiper-bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSuggestion from 'vue-suggestion'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueSuggestion)
 
 Vue.config.productionTip = false
+
+Vue.mixin({
+  methods: {
+    getAge (birth_year) {
+      const today = new Date();
+      const age = today.getFullYear() - birth_year
+      return `${age} years old`
+    }
+  }
+})
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import AddPatient from '../views/AddPatient.vue'
+import PatientDetails from '../views/PatientDetails.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -11,12 +16,24 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/addPatient',
+    name: 'AddPatient',
+    component: AddPatient
+  },
+  {
+    path: '/patient',
+    name: 'PatientDetail',
+    component: PatientDetails
   }
 ]
 
@@ -24,6 +41,17 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'Register'  && !localStorage.access_token) {
+    next()
+  } else if (to.name !== 'Login'  && !localStorage.access_token) {
+    next({ name: 'Login' })
+  } else {
+    next()
+    store.commit('SET_IS_LOGGED_IN', true)
+  }
 })
 
 export default router
