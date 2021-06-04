7047d68401f83fa465a6f3fbec36f9bda965ae0b
Teddy Pratama Riska <tedypr.109@gmail.com>
2021-06-04 10:57:13 +0700

    add icon

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="card-body col-md-3">
-        <div class="card text-success mb-3 m-3" style="width: 20vmax;">
+        <div class="card text-success mb-3 m-3" style="width: 21vmax;">
             <h3 class="card-header"><i class="fas fa-play"></i> Now Playing</h3>
             <img :src="nowPlaying.img" alt="" srcset="">
             <div class="card-body">
                 <p class="card-text text-danger">{{nowPlaying.title}}</p>
             </div>
-            <div class="card-body">
-                <a :href="nowPlaying.url" class="card-link">Show Schedule</a>
-                <a @click.prevent="watchList(nowPlaying.title)" href="" class="card-link hover">Add Watch List</a>
+            <div class="card-body mb-2">
+                <a :href="nowPlaying.url" class="card-link"><i class="fas fa-tv"></i> Show Schedule</a>
+                <a @click.prevent="watchList(nowPlaying.title)" href="" class="card-link hover"><i class="fas fa-plus"></i> Add Watch List</a>
             </div>
             <div class="card-footer text-success">
                 Now Playing
