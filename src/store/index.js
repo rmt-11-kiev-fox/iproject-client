dd0231257cc72a32a31bbae266339d7a6d898518
Hanun Shahreza <hanunshahreza@Hanuns-MacBook-Air.local>
2021-06-04 08:53:47 +0700

    done register login feature

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,88 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '../router'
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3000'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isLogin: false,
+    homeData: []
   },
   mutations: {
+    SET_LOGIN (state, data) {
+      state.isLogin = data
+    },
+    SET_HOME (state, data) {
+      state.homeData = data
+    }
   },
   actions: {
+    register (context, payload) {
+      axios({
+        method: 'POST',
+        url: `${baseURL}/register`,
+        data: {
+          username: payload.username,
+          password: payload.password
+        }
+      }).then(response => {
+        console.log(response.data)
+        router.push({ path: '/login' })
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    login (context, payload) {
+      axios({
+        method: 'POST',
+        url: `${baseURL}/login`,
+        data: {
+          username: payload.username,
+          password: payload.password
+        }
+      }).then(response => {
+        console.log(response.data)
+        localStorage.setItem('access_token', response.data.access_token)
+        context.commit('SET_LOGIN', true)
+        router.push({ path: '/' })
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    showReport (context, data) {
+      axios({
+        method: 'GET',
+        url: `${baseURL}/reports`,
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        },
+        data: {
+          period: data
+        }
+      }).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    homepageData (context) {
+      axios({
+        method: 'GET',
+        url: `${baseURL}/`
+      }).then(response => {
+        console.log(response.data)
+        context.commit('SET_HOME', response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
   },
   modules: {
   }
---
+++ src/views/Report.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <b-form-select class="my-3" v-model="selected" :options="options"></b-form-select>
+    <!-- <b-form-select v-model="selected" :options="options" size="sm" class="mt-3"></b-form-select> -->
+    <div class="mt-3">
+        Selected:
+        <strong>{{ selected }}</strong><br>
+        <button @click.prevent="showReport" class="btn btn-default btn-primary" >Show Reports</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Report',
+  data () {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: 'Please select month' },
+        { value: 'JAN', text: 'January' },
+        { value: 'FEB', text: 'February' },
+        { value: 'MAR', text: 'March' },
+        { value: 'APR', text: 'April' },
+        { value: 'MAY', text: 'May' },
+        { value: 'JUN', text: 'June' },
+        { value: 'JUL', text: 'July' },
+        { value: 'AUG', text: 'August' },
+        { value: 'SEP', text: 'September' },
+        { value: 'OCT', text: 'October' },
+        { value: 'NOV', text: 'November' },
+        { value: 'DEC', text: 'December' }
+      ]
+    }
+  },
+  methods: {
+    showReport () {
+      const period = this.selected
+      this.$store.dispatch('showReport', period)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
